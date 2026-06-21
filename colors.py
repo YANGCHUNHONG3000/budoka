9bfc13c4514d9bac495cfaf59b75618cab248025
Code Monkey King <freesoft.for.people@gmail.com>
2026-06-21 19:23:33 +0300

    fixed black belt gb color

--- colors.py
+++ colors.py
@@ -6,7 +6,7 @@ curses.init_pair(1, curses.COLOR_RED, -1),
 curses.init_pair(2, curses.COLOR_CYAN, -1),
 curses.init_pair(3, curses.COLOR_BLUE, -1),
 curses.init_pair(4, curses.COLOR_GREEN, -1),
-curses.init_pair(5, curses.COLOR_BLACK, -1),
+curses.init_pair(5, curses.COLOR_BLACK, curses.COLOR_WHITE),
 curses.init_pair(6, curses.COLOR_WHITE, -1),
 curses.init_pair(7, curses.COLOR_YELLOW, -1),
 curses.init_pair(8, curses.COLOR_MAGENTA, -1)
